e821068666b4e7c8bd0f4111fb9ac2c73808f84e
Aidan Mulholland <aidanmulholland89@gmail.com>
2023-12-06 10:28:02 +0000

    feature: parses non empty bodys to strings and returns the body. Pre requisite to parsing xml

--- src/functions/tempendpoint.ts
+++ src/functions/tempendpoint.ts
@@ -1,14 +1,22 @@
 import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
 
 export async function tempendpoint(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
+  const stream = request.body;
+  if (!stream) {
+    return { body: `Empty body` };
+  }
 
-    console.log(request);
+  const chunks = [];
+  for await (const chunk of stream) {
+    chunks.push(Buffer.from(chunk));
+  }
 
-    return { body: `OK!` };
-};
+  const data = Buffer.concat(chunks).toString("utf-8");
+  return { body: data };
+}
 
-app.http('tempendpoint', {
-    methods: ['GET', 'POST'],
-    authLevel: 'function',
-    handler: tempendpoint
+app.http("tempendpoint", {
+  methods: ["GET", "POST"],
+  authLevel: "function",
+  handler: tempendpoint,
 });
