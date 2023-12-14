5ced360efc7829e63feadf19f57d6b8372382076
Aidan Mulholland <aidanmulholland89@gmail.com>
2023-12-14 10:40:43 +0000

    fix: reconnected egalaxy request to system

--- src/services/ticketService.ts
+++ src/services/ticketService.ts
@@ -54,50 +54,17 @@ export class TicketService {
       </Body>
     </Envelope>`;
 
-    const res = await fetch("https://tickets.midway.org/eGalaxy/eGalaxy.ashx", {
+    const res = await fetch("http://174.78.189.50:3051", {
       method: "POST",
       headers: { "content-type": "text/xml" },
       body: eGalaxyRequest,
     });
 
-    // if (!res.ok) {
-    //   throw Error(await res.text());
-    // }
+    if (!res.ok) {
+      throw Error(await res.text());
+    }
 
-    const incomingXMLString = `<?xml version="1.0" encoding="UTF-8"?>
-    <Envelope>
-      <Header>
-        <MessageID>0</MessageID>
-        <MessageType>GetEventsResponse</MessageType>
-        <SourceID>1</SourceID>
-        <TimeStamp>2023-12-06 12:48:48</TimeStamp>
-        <EchoData></EchoData>
-        <SystemFields></SystemFields>
-      </Header>
-      <Body>
-        <Events>
-          <Event>
-            <ResponseCode>0</ResponseCode>
-            <EventID>243</EventID>
-            <StartDateTime>${new Date(new Date().getDate() + 3)}</StartDateTime>
-            <EndDateTime>2017-08-27 15:30:00</EndDateTime>
-            <EventTypeID>1</EventTypeID>
-            <OnSaleDateTime>2017-07-23 10:00:00</OnSaleDateTime>
-            <OffSaleDateTime>2017-08-27 14:50:00</OffSaleDateTime>
-            <ResourceID>1</ResourceID>
-            <UserEventNumber>0</UserEventNumber>
-            <Available>300</Available>
-            <TotalCapacity>300</TotalCapacity>
-            <Status>1</Status>
-            <HasRoster>NO</HasRoster>
-            <RSEventSeatMap>0</RSEventSeatMap>
-            <PrivateEvent>NO</PrivateEvent>
-            <HasHolds>NO</HasHolds>
-            <EventName>Dinosaurs 3-D</EventName>
-          </Event>
-        </Events>
-      </Body>
-    </Envelope>`;
+    const incomingXMLString = await res.text();
 
     const parsedResponse = await new Promise<GetEventResponse[]>((resolve, reject) => {
       parseString(incomingXMLString, { explicitArray: false }, (err: Error, result: EventData) => {
